68c97f81662f0680581195c7956606f08bc34525
Guilherme-Marcionilo <guilhermemarcionilo@gmail.com>
2020-11-03 14:28:23 -0300

    APIREST com SpringBoot

---
+++ Modulo II - Banco de dados/db_produtos_apirest.sql
@@ -0,0 +1,11 @@
+use produtos_apirest;
+describe tb_produtos;
+
+insert into tb_produtos values
+(2, 'SAMSUNG S9', 1.00,2599.00),
+(3, 'REDMI NOTE 8', 1.00,9599.00),
+(4, 'SAMSUNG S10', 1.00,1599.00),
+(5, 'SAMSUNG J1 MINI', 1.00,2.00);
+
+select * from tb_produtos;
+
