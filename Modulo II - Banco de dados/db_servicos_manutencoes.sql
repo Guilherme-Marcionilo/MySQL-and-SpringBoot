e82369e1f43e191c4158b18b7ce30bfc4af66f38
Guilherme-Marcionilo <guilhermemarcionilo@gmail.com>
2020-11-06 18:24:03 -0300

    Criando um serviço de manutenções, usando métodos os métodos: GET,POST,DELETE,PUT

---
+++ Modulo II - Banco de dados/db_servicos_manutencoes.sql
@@ -0,0 +1,9 @@
+use servicos_manutencoes;
+
+select * from tb_manutencao;
+
+insert into tb_manutencao values
+(default,'2020-03-05','Junior',true),
+(default,'2020-03-05','Cleitin',true),
+(default,'2020-03-05','Borges',true),
+(default,'2020-03-05','Monique',true);
